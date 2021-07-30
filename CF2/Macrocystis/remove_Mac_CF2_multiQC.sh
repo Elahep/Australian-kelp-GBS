915c3c74c236078768250eb9dfdf5720226fa411
Elahe Parvizi <Elahep@users.noreply.github.com>
2021-07-31 11:04:04 +1200

    Add files via upload

---
+++ CF2/Macrocystis/remove_Mac_CF2_multiQC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+rm	Mac4ml36.fq.gz
+rm	Mac4ml41.fq.gz
+rm	Mac4ml42.fq.gz
+rm	MacEdn1.fq.gz
+rm	MacEdn2.fq.gz
+rm	MacFish4.fq.gz
+rm	MacFish5.fq.gz
+rm	MacFish7.fq.gz
+rm	MacFish8.fq.gz
+rm	MacKey1.fq.gz
+rm	MacKey2.fq.gz
+rm	MacKey3.fq.gz
+rm	MacKey5.fq.gz
+rm	MacKey6.fq.gz
+rm	MacKey8.fq.gz
+rm	MacShel10.fq.gz
+rm	MacShel11.fq.gz
+rm	MacShel13.fq.gz
+rm	MacShel14.fq.gz
+rm	MacShel6.fq.gz
+rm	MacShel8.fq.gz
+rm	MacShel9.fq.gz
