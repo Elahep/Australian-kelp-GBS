5cf162c365c7450ae0a32db0870b2d5951715aeb
Elahe Parvizi <Elahep@users.noreply.github.com>
2021-08-01 14:06:39 +1200

    Add files via upload

---
+++ CF2/potatorum/toremove_pot_CF2_multiQC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##samples removed after MultiQC quality control step. These samples had less than 0.5 million raw reads.
+
+rm	potLady7.fq.gz
+rm	potPas29.fq.gz
+rm	potPas30.fq.gz
+rm	potPas31.fq.gz
+rm	potPas34.fq.gz
+rm	potPas35.fq.gz
